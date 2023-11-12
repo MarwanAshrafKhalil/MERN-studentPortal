febc731fe10a1ef8a3203520cacf8ead99fbc17b
MarwanAshrafKhalil <marwan.a.k.1997@gmail.com>
2023-11-12 08:00:48 +0200

    announcements styling

---
+++ client/src/components/announcements/Announcements.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./_announcements.scss";
+import { AccountCircle } from "@mui/icons-material";
+
+function Announcements() {
+  return (
+    <div className="announce">
+      <div className="announce__title">
+        <h2>Announcements</h2>
+        <p>These are the recents announcements</p>
+      </div>
+
+      <div className="announce__data">
+        <div className="announce__data__single">
+          <AccountCircle className="prof__icon" style={{ fontSize: "48px" }} />
+
+          <div className="announce__data__single__prof">
+            <p>Mr.Ahmed Mostafa</p>
+            <p className="course">Math 101</p>
+          </div>
+          <div className="announce__data__single__content">
+            <p>
+              Reminder: The deadline for submitting your research paper is
+              approaching. Please ensure you submit it by November 15th.
+            </p>
+          </div>
+        </div>
+
+        <div className="announce__data__single">
+          <AccountCircle className="prof__icon" style={{ fontSize: "48px" }} />
+
+          <div className="announce__data__single__prof">
+            <p>Mr.Ahmed Mostafa</p>
+            <p className="course">Math 101</p>
+          </div>
+          <div className="announce__data__single__content">
+            <p>
+              Attention students: The guest lecture scheduled for tomorrow has
+              been postponed. A new date will be announced soon.
+            </p>
+          </div>
+        </div>
+
+        <div className="announce__data__single">
+          <AccountCircle className="prof__icon" style={{ fontSize: "48px" }} />
+
+          <div className="announce__data__single__prof">
+            <p>Mr.Ahmed Mostafa</p>
+            <p className="course">Math 101</p>
+          </div>
+          <div className="announce__data__single__content">
+            <p>
+              Important notice: The registration for next semester's courses
+              will open on November 20th. Plan your schedule accordingly.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Announcements;
--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./dashboard.scss";
 import { Col, Row } from "react-bootstrap";
 import Hero from "../../components/hero/Hero";
+import Announcements from "../../components/announcements/Announcements";
 
 function Dashboard() {
   return (
@@ -10,7 +11,8 @@ function Dashboard() {
 
       <Row>
         <Col xs={12} md={8}>
-          <div className="col-content dashboard__announ">Content for col 8</div>
+          <Announcements />
+          {/* <div className="col-content dashboard__announ">Content for col 8</div> */}
         </Col>
         <Col xs={12} md={4}>
           <div className="col-content dashboard__tasks">Content for col 4</div>
